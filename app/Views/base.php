939048f96b80ea05e4ddca04af1bda401c509594
ale <alearroyoo@gmail.com>
2021-04-28 10:07:55 -0600

    Eliminar fecha

--- app/Views/base.php
+++ app/Views/base.php
@@ -28,9 +28,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <h3>Hoy es <?=fechaCastellano(date("Y-m-d"))?></h3>
-      </li>
+     
     </ul>
 
     
